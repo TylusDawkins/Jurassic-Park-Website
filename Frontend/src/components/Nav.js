c3380d85a93fe20abfb5bdfe1bbabe46d5987d2b
rarebit3 <johntrevorbond@gmail.com>
2022-03-25 10:32:35 -0400

    switched to react router and working

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -6,13 +6,14 @@ const Nav = () => {
   let navigate = useNavigate()
   
   return (
-    <nav className="navbar">
+    <nav id="navbar">
+        <img src='https://i.imgur.com/xxlw1er.png' alt="Jurassic Logo" id="logo" />
       <h4>Jurassic Park</h4>
       <div>
         <Link to='/'>Home</Link>
         <Link to='/joinform'>Join Our Newsletter!</Link>
         <Link to='/attractions'>Attractions</Link>
-        <Link tp='/aboutus'>Our Staff</Link>
+        <Link to='/aboutus'>Our Staff</Link>
         <div className='bckbtn' onClick={() => navigate(-1)}>Back</div>
       </div>
     </nav>
