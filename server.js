dfd935ead9f34c19cb84d55e31c91031b45eaed2
tiffpereira <tiffanympereira@gmail.com>
2022-03-25 11:40:42 -0400

    refactoring files

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.use(bodyParser.json());
 
 app.use('/api', routes);
 
+app.use(express.static(`${__dirname}/client/build`))
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+   })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
